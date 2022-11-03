b1b482c3733cf1989ab6e039138ff4b7c8e3d262
Agam789 <93301113+Agam789@users.noreply.github.com>
2022-11-03 21:29:20 +0530

    My Day-1(First File)
    Program On AREA OF A RECTANGLE

---
+++ day1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int length,width;
+  printf("Enter Your length");
+  scanf("%d",&length);
+  printf("Enter The Value Of Width");
+  scanf("%d",&width);
+  printf("The Perimeter Be%d"(length*width));
+  return 0;
+}
