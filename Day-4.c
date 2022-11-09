860f40981ed72ce924ce490a6494d41e52506648
Agam789 <93301113+Agam789@users.noreply.github.com>
2022-11-09 20:07:01 +0530

    Create Day-4.c
    Asrama Pranali (#Pranali)

---
+++ Day-4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  int age;
+  printf("Please Enter Your Age = ");
+  scanf("%d",&age);
+  if(age<25){
+    printf("You Are In Brahmacharya #STUDENT LIFE");
+  }
+    else if(age<48){
+      printf("You Are In A Grihastha #HouseHold Life");
+    }
+    else if(age<72){
+      printf("You Are In A Vanaprastha #Retired Life");
+    }
+  else{
+    printf("Sannyashi #Sanyashi");
+  }
+  return 0;
+}
